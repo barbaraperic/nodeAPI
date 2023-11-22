8a03ff8da5f4d39e7bfd26ddb340d2f9126f175b
Barbara Peric <b.peric10@gmail.com>
2023-11-22 20:30:01 +0000

    create and delete handler

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -27,3 +27,45 @@ export const getOneProduct = async (req, res) => {
 	});
 	return res.json({ data: product });
 };
+
+export const createProduct = async (req, res) => {
+	const product = await prisma.product.create({
+		data: {
+			name: req.body.name,
+			belongsToId: req.user.id,
+		},
+	});
+
+	return res.json({ data: getOneProduct });
+};
+
+// update
+
+export const updateProduct = async (req, res) => {
+	const updated = await prisma.product.update({
+		where: {
+			id_belongsToId: {
+				id: req.params.id,
+				belongsToId: req.user.id,
+			},
+		},
+		data: {
+			name: req.body.name,
+		},
+	});
+	return res.json({ data: updated });
+};
+
+// delete
+
+export const deleteProduct = async (req, res) => {
+	const deleted = await prisma.product.delete({
+		where: {
+			id_belongsToId: {
+				id: req.params.id,
+				belongsToId: req.user.id,
+			},
+		},
+	});
+	return res.json({ data: deleted });
+};
